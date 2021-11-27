b693d1573724673f2c7a46e3abe2d8321c2084ff
4rgc <bohdan.andrii@gmail.com>
2021-11-27 14:25:26 -0500

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () => (
+	<header className="App-header">
+		<div className="header-text">Great ticket viewer</div>
+	</header>
+);
+
+export default Header;
