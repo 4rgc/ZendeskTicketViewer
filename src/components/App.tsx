1009b1df8bf79fc8eae045c29fa8e23115021912
4rgc <bohdan.andrii@gmail.com>
2021-11-28 18:08:52 -0500

    feat: format the ticket data before displaying it

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { Column } from 'react-table';
 import useSWR from 'swr';
 import { ZendeskTicket } from '../types/ZendeskTicket';
@@ -6,20 +6,38 @@ import './App.css';
 import Header from './Header/Header';
 import Table from './Table/Table';
 import { TicketDetails } from './TicketDetails/TicketDetails';
+import _ from 'lodash';
+import moment from 'moment';
 
 const fetcher = (input: RequestInfo) => fetch(input).then((res) => res.json());
+type APIResponse = {
+	tickets?: ZendeskTicket[];
+	error?: string;
+};
 
 function App() {
 	const apiUrl =
 		'http://ec2-35-183-81-115.ca-central-1.compute.amazonaws.com:8080/tickets?limit=25';
 
-	const { data, error } = useSWR<{
-		tickets?: ZendeskTicket[];
-		error?: string;
-	}>(apiUrl, fetcher);
+	const { data, error } = useSWR<APIResponse>(apiUrl, fetcher);
+	const [displayData, setDisplayData] = useState<APIResponse['tickets']>();
 	const [selectedTicket, setSelectedTicket] = useState<ZendeskTicket | null>(
 		null
 	);
+
+	useEffect(() => {
+		if (data) {
+			const tickets = data.tickets;
+			if (tickets) {
+				const clonedTickets = _.cloneDeep(tickets);
+				clonedTickets.forEach((ticket) => {
+					ticket.created_at = moment(ticket.created_at).calendar();
+					ticket.updated_at = moment(ticket.updated_at).calendar();
+				});
+				setDisplayData(clonedTickets);
+			}
+		}
+	}, [data]);
 
 	const tableHeaders: Column<Partial<ZendeskTicket>>[] = [
 		{
@@ -56,9 +74,7 @@ function App() {
 			<Header />
 			<div className="table-container">
 				<Table
-					data={
-						data && !data.error && data.tickets ? data.tickets : []
-					}
+					data={displayData ? displayData : []}
 					columns={tableHeaders}
 					onRowClick={onRowClick}
 				/>
