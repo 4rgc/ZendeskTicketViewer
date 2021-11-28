98b8f08a1ef5540d5d11ba8d5fae22a2e2138724
4rgc <bohdan.andrii@gmail.com>
2021-11-27 21:29:58 -0500

    feat: test the Table in the App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,39 @@
+import useSWR from 'swr';
 import './App.css';
 import Header from './Header/Header';
+import Table from './Table/Table';
+
+const fetcher = (input: RequestInfo) => fetch(input).then((res) => res.json());
 
 function App() {
+	const apiUrl =
+		'http://ec2-35-183-81-115.ca-central-1.compute.amazonaws.com:8080/tickets?limit=5';
+
+	const { data, error } = useSWR(apiUrl, fetcher);
+
+	const tableHeaders = [
+		{
+			Header: 'Subject',
+			accessor: 'subject',
+		},
+		{
+			Header: 'Status',
+			accessor: 'status',
+		},
+		{
+			Header: 'Created At',
+			accessor: 'created_at',
+		},
+		{
+			Header: 'Updated At',
+			accessor: 'updated_at',
+		},
+	];
+
 	return (
 		<div className="App">
 			<Header />
+			<Table data={data ? data.tickets : []} columns={tableHeaders} />
 		</div>
 	);
 }
