341aa2298ba7b938189c9ff367f0bed426fdf4ea
4rgc <bohdan.andrii@gmail.com>
2021-11-29 20:16:23 -0500

    test: add tests for the ErrorView

---
+++ src/test/ErrorView/ErrorView.test.tsx
@@ -0,0 +1,24 @@
+import { render } from 'enzyme';
+import { ErrorView } from '../../components/ErrorView/ErrorView';
+
+describe('ErrorView tests', () => {
+	it('should render the message as an error', () => {
+		const error = render(
+			<ErrorView
+				type="error"
+				message="This is an error. Please contact support"
+			/>
+		);
+		expect(error).toMatchSnapshot();
+	});
+
+	it('should render the message as a warning', () => {
+		const warning = render(
+			<ErrorView
+				type="warning"
+				message="This is a warning. Please try again"
+			/>
+		);
+		expect(warning).toMatchSnapshot();
+	});
+});
