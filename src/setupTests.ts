67f50c20db162ce5ba913b05e74246143168c27e
4rgc <bohdan.andrii@gmail.com>
2021-11-29 22:24:41 -0500

    fix: useLayoutEffect can't be used on server error

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,8 @@ import '@testing-library/jest-dom';
 import { setGlobalConfig } from '@storybook/testing-react';
 // Storybook's preview file location
 import * as globalStorybookConfig from '../.storybook/preview';
+import React from 'react';
+React.useLayoutEffect = React.useEffect;
 
 setGlobalConfig(globalStorybookConfig);
 
