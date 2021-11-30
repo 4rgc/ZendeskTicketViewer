cee4614333a97d9c31219362a61f84106c4c14e9
4rgc <bohdan.andrii@gmail.com>
2021-11-29 21:02:42 -0500

    fix: config files to work with stories

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom';
+import { setGlobalConfig } from '@storybook/testing-react';
+// Storybook's preview file location
+import * as globalStorybookConfig from '../.storybook/preview';
+
+setGlobalConfig(globalStorybookConfig);
 
 Enzyme.configure({ adapter: new Adapter() });
