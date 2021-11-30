2fab6da1d0a661d31f8773b2a37602bb0e4de029
4rgc <bohdan.andrii@gmail.com>
2021-11-29 21:53:06 -0500

    test: cover the Paging component

---
+++ src/test/Paging/Paging.test.tsx
@@ -0,0 +1,84 @@
+import { shallow } from 'enzyme';
+import { Paging, PagingProps } from '../../components/Paging/Paging';
+
+describe('Paging tests', () => {
+	const defaultArgs: PagingProps = {
+		pageCount: 0,
+		page: 0,
+		size: 'small',
+		onPageSelected: () => {},
+		onNextSelected: () => {},
+		onPrevSelected: () => {},
+	};
+
+	beforeAll(() => {});
+
+	it('should render 2 Buttons', () => {
+		const paging = shallow(<Paging {...defaultArgs} showPrevNext={true} />);
+		expect(paging.find('Button').length).toBe(2);
+	});
+
+	it('should render 5 buttons', () => {
+		const paging = shallow(<Paging {...defaultArgs} pageCount={5} />);
+		expect(paging.find('Button').length).toBe(5);
+	});
+
+	it('should render limiting to 5 buttons', () => {
+		const paging = shallow(
+			<Paging {...defaultArgs} pageCount={5} size="large" />
+		);
+		expect(paging.find('Button').length).toBe(5);
+	});
+
+	it('should render 7 buttons', () => {
+		const paging = shallow(
+			<Paging {...defaultArgs} pageCount={7} size="large" />
+		);
+		expect(paging.find('Button').length).toBe(7);
+	});
+
+	it('should pass the previous button handler to the button', () => {
+		const mockHandler = jest.fn(() => {});
+		const paging = shallow(
+			<Paging
+				{...defaultArgs}
+				showPrevNext={true}
+				page={1}
+				pageCount={3}
+				onPrevSelected={mockHandler}
+			/>
+		);
+		paging.find('Button').first().simulate('click');
+		expect(mockHandler).toHaveBeenCalled();
+	});
+
+	it('should pass the next button handler to the button', () => {
+		const mockHandler = jest.fn(() => {});
+		const paging = shallow(
+			<Paging
+				{...defaultArgs}
+				showPrevNext={true}
+				page={1}
+				pageCount={3}
+				onNextSelected={mockHandler}
+			/>
+		);
+		paging.find('Button').last().simulate('click');
+		expect(mockHandler).toHaveBeenCalled();
+	});
+
+	it('should pass the page selected button handler to the button', () => {
+		const mockHandler = jest.fn(() => {});
+		const paging = shallow(
+			<Paging
+				{...defaultArgs}
+				showPrevNext={true}
+				page={1}
+				pageCount={3}
+				onPageSelected={mockHandler}
+			/>
+		);
+		paging.find('Button').at(1).simulate('click');
+		expect(mockHandler).toHaveBeenCalled();
+	});
+});
---
+++ src/test/Paging/PagingStories.test.tsx
@@ -0,0 +1,50 @@
+import {
+	OnePage,
+	TwoPage,
+	ThreePage,
+	FivePage,
+	SevenPage,
+	TenPage,
+	SevenPageWithPrevNext,
+	SevenPageOnLast,
+} from '../../components/Paging/Paging.stories';
+import { render } from 'enzyme';
+import { PagingProps } from '../../components/Paging/Paging';
+
+describe('Paging story tests', () => {
+	it('should render one page Paging', () => {
+		const args = OnePage.args as PagingProps;
+		const paging = render(<OnePage {...args} />);
+		expect(paging).toMatchSnapshot();
+	});
+	it('should render two page Paging', () => {
+		const args = TwoPage.args as PagingProps;
+		const paging = render(<TwoPage {...args} />);
+		expect(paging).toMatchSnapshot();
+	});
+	it('should render five page Paging', () => {
+		const args = FivePage.args as PagingProps;
+		const paging = render(<FivePage {...args} />);
+		expect(paging).toMatchSnapshot();
+	});
+	it('should render seven page Paging', () => {
+		const args = SevenPage.args as PagingProps;
+		const paging = render(<SevenPage {...args} />);
+		expect(paging).toMatchSnapshot();
+	});
+	it('should render ten page Paging', () => {
+		const args = TenPage.args as PagingProps;
+		const paging = render(<TenPage {...args} />);
+		expect(paging).toMatchSnapshot();
+	});
+	it('should render seven page Paging with prev/next', () => {
+		const args = SevenPageWithPrevNext.args as PagingProps;
+		const paging = render(<SevenPageWithPrevNext {...args} />);
+		expect(paging).toMatchSnapshot();
+	});
+	it('should render seven page Paging on last page', () => {
+		const args = SevenPageOnLast.args as PagingProps;
+		const paging = render(<SevenPageWithPrevNext {...args} />);
+		expect(paging).toMatchSnapshot();
+	});
+});
